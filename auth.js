80924f09acb8ed518ad64c2560d0e4449d3dfa8c
tama <naotama110@gmail.com>
2026-03-26 18:10:27 +0900

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 (function () {
-  const GAS_PROXY_URL = 'https://script.google.com/macros/s/AKfycbzh2CbWnlI6VgJ1nWKFsh2XiuHda-br1GJCkgxLFMaEwbASxlSyKC1zv0ARIMyKVWpFnA/exec';
+  const GAS_PROXY_URL = 'https://script.google.com/macros/s/AKfycbxBWtvhWk5BdvoILihyqYZDLJrDjuVFRUB0t_D7DeNe3mlXgAAcnvgznk9mYLFYwxKg/exec';
   const SESSION_TOKEN_KEY = 'keio_navi_session_token_v1';
   const USER_CACHE_KEY = 'keio_navi_current_user_cache_v1';
   const LIKED_CACHE_KEY = 'keio_navi_liked_cache_v1';
